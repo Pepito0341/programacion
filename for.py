f9fbc2859e86bddc92819bfa59465e6803ccc87c
Pepito0341 <pepitocastaneda15@gmail.com>
2025-01-08 21:54:01 -0300

    si

--- for.py
+++ for.py
@@ -1,4 +1,5 @@
 from os import system
+import random
 system("cls")
 # for i in range(1,4):
 #     print(i)
@@ -22,9 +23,46 @@ system("cls")
 #     num=float(input())
 #     total=total+num
 # print(total/3)
-total=0
-for i in range(3):
-    print("ingrese una nota")
-    nota=int(input())
-    total=total+nota
-print("Su promedio es ", total/(i+1))
+# total=0
+# for i in range(3):
+#     print("ingrese una nota")
+#     nota=int(input())
+#     total=total+nota
+# print("Su promedio es ", total/(i+1))
+# e = int(input("ingrese un numero entero: "))
+# for i in range(1,e+1):
+#     if i% 2:
+#         print(f"El numero {i} es impar")
+#     else:
+#         print("El numero es par")
+# pal=input("ingrese su nombre")
+# total=0
+# for i in pal:
+#     print(i)
+# pal=input("ingrese su nombre")
+# for i in range(len(pal)):
+#     t=t+i+1
+#     print(f"total= {t}")
+# contraseña=int(input("ingrese una contraseña"))
+# cont=2
+# for i in range(3):
+#     con=int(input("ingrese su contraseña: "))
+#     if contraseña==con:
+#         print("bienvenido al sistema")
+#         break
+#     else:
+#         print(f"Le quedan {cont} intentos")
+#         cont=cont-1
+numero = random.randint(1,10)
+
+for i in range(5):
+    c=input("ingrese un numero entre el 1 y el 10")
+    if c==numero:
+        print("felicidades adivinaste el numero")
+        break
+    elif c<numero:
+        print("el numero esta muy bajo")
+    elif c> numero:
+        print("el numero esta muy alto")
+    else:
+        print("no adivino")
